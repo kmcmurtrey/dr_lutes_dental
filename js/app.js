07edbe7176fcc619457341cdff57ad32506b62e5
kmcmurtrey <korey.mcmurtrey@gmail.com>
2015-03-12 22:55:22 -0400

    Add responsive mobile nav icon

---
+++ js/app.js
@@ -0,0 +1,24 @@
+//$(funtion() {
+//  var pull = $('#pull');
+//  var menu = $('nav ul');
+//  var menuHeight = menu.height();
+//  
+//  
+//  $(pull).on('click', function(e) {
+//    e.preventDefault();
+//    menu.slideToggle();
+//  });
+//});
+var menu = $('nav ul');
+
+$('#pull').click(function(e) {
+  e.preventDefault();
+  menu.slideToggle();
+});
+
+$(window).resize(function() {
+  var w = $(window).width();
+  if(w > 480 && menu.is(':hidden')) {
+    menu.removeAttr('style');
+  }
+})
