e013c26914d25c8a1d6f3f4971e6a879036708c8
kmcmurtrey <korey.mcmurtrey@gmail.com>
2015-03-26 22:40:16 -0400

    Add comments to slideShow function

--- js/app.js
+++ js/app.js
@@ -21,14 +21,17 @@ function slideSwitch() {
   var $active = $('#slideshow img.active');
   var $next;
   
+  //Loop the $next image back to the beginning of the stack
   if ($active.next().length !== 0) {
     $next = $active.next();
   } else {
     $next = $('#slideshow img').first();
   }
   
+  //Add .last-active so the .active img will fade back with a lower z-index
   $active.addClass('last-active');
   
+  //Add .active to $next img. Use animate() to change the opacity and remove the classes from $active
   $next.css({opacity: 0.0})
     .addClass('active')
     .animate({opacity: 1.0}, 1000, function() {
@@ -36,6 +39,7 @@ function slideSwitch() {
   })  
 }
 
+//Call the slideSwitch() function every 5 seconds
 $(function() {
   setInterval(slideSwitch, 5000);
 })
